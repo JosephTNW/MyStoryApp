9af1e3987cd1d6464f08d8f4d189d29f9ea441e3
JosephTNW <josephtedjanw@gmail.com>
2022-10-08 15:07:31 +0700

    pre final (TODO: stack widget)

---
+++ app/src/main/java/com/example/mystoryapp/utils/Constants.kt
@@ -0,0 +1,9 @@
+package com.example.mystoryapp.utils
+
+object Constants {
+    const val MAIN_URL = "https://story-api.dicoding.dev/v1/"
+    const val DETAIL_STORY = "story"
+    const val TOKEN_KEY = "token_key"
+    const val LOGIN_INFO = "login_info"
+    const val NO_TOKEN = "no_token"
+}
